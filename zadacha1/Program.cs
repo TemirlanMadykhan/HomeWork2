400c28be846b842ce66d26db837471569349fca3
Temirlan Madykhan <tima.001@mail.ru>
2023-02-02 20:00:06 +0500

    HomeWork2

---
+++ zadacha1/Program.cs
@@ -0,0 +1,7 @@
+﻿Random random = new Random();
+int min = 100;
+int max = 999;
+int number = random.Next(min, max + 1);
+Console.WriteLine(number);
+int a = number / 10 % 10;
+Console.WriteLine(a);
---
+++ zadacha2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Random random = new Random();
+// int min = 100;
+// int max = 999;
+// int number = random.Next(min, max + 1);
+// Console.WriteLine(number);
+// int a = number / 10;
+// int b = number % 10;
+// Console.WriteLine(b);
+
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
